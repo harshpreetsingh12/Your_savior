17f902bb8608277e94573a245c72bc7c33ac318f
unknown <problemsolver131@gmail.com>
2024-06-09 14:42:38 +0530

    env prot

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import * as firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYWhk5NwZp-blKkKPVMBZ9nn7qibOkLLI",
+  apiKey: process.env.REACT_FIREBASE_API_KEY,
   authDomain: "your-savior.firebaseapp.com",
   projectId: "your-savior",
   storageBucket: "your-savior.appspot.com",
   messagingSenderId: "12161338293",
-  appId: "1:12161338293:web:2042e984bf635f721f67fc",
+  appId: process.env.REACT_FIREBASE_APP_ID,
   measurementId: "G-LCHX9SLWH8"
 };
 // Initialize Firebase
